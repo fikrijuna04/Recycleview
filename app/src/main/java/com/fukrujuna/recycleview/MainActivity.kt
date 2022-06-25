d3fa10759c243364d64cd6d7c8374a7b19edfb45
fikri <fikrijunaedi004@@gmail.com>
2022-06-25 14:27:28 +0700

    Recycleview app

---
+++ app/src/main/java/com/fukrujuna/recycleview/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.fukrujuna.recycleview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.fukrujuna.recycleview.adapter.Adaptermadrid
+import com.fukrujuna.recycleview.databinding.ActivityMainBinding
+import com.fukrujuna.recycleview.model.pemain
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val listpemain = ArrayList<pemain>()
+        listpemain.add(pemain("thibaut courtuis",R.drawable.courtois,"penjaga gawang","2.00 m","belgiaa"," 10 juni 1990"))
+        listpemain.add(pemain("Karim Benzema",R.drawable.benzema,"penyerang","1.80 m","perancis","18 desember 1989"))
+        listpemain.add(pemain("Marcelo",R.drawable.marcello,"bek","1.70 m","brazil","26 okto 1999"))
+        listpemain.add(pemain("Ramos sergi",R.drawable.ramos,"bek","2.00 m","spain","23 january 1992"))
+        listpemain.add(pemain("zidane zidane",R.drawable.zidan,"pelatih","1.50 m","prancis","17 juni  1980"))
+        binding.List.adapter = Adaptermadrid(this,listpemain,object : Adaptermadrid.onClickListener{
+            override fun detailData(item: pemain?) {
+                TODO("Not yet implemented")
+            }
+
+        })
+
+    }
+
+}
---
+++ app/src/main/java/com/fukrujuna/recycleview/model/pemain.kt
@@ -0,0 +1,10 @@
+package com.fukrujuna.recycleview.model
+
+data class pemain(
+    var nama : String,
+    var foto : Int,
+    var posisi : String,
+    var tinggi : String,
+    var tempatlahir : String,
+    var tgllahir : String
+)
